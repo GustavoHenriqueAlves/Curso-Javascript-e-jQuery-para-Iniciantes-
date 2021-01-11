f9c54827a95bd403f0c14051bff95054029bd44b
Gustavo Henrique Alves <he.gustavoalves@gmail.com>
2021-01-10 21:38:47 -0300

    Exclusao de funçoes desnecessarias e criaçao de funçoes para mostrar e esconder mensagens referentes as alteraçoes de cores

--- script.js
+++ script.js
@@ -7,17 +7,26 @@ $(function () {
 
     $('#mostrar').click(function () {
 
-        $('h1').delay(3000);
         $('h1').fadeIn();
     });
 
     $('#vermelho').click(function () {
 
         $('h1').css('color', 'red');
+        $('span').text('Cor alterada para vermelho');
+        $('span').fadeIn();
+        $('span').css('color', 'red');
+        $('span').delay(1500);
+        $('span').fadeOut('slow');
     });
 
     $('#azul').click(function () {
 
         $('h1').css('color', 'blue');
+        $('span').text('Cor alterada para azul');
+        $('span').fadeIn();
+        $('span').css('color', 'blue');
+        $('span').delay(1500);
+        $('span').fadeOut('slow');
     });
 });
